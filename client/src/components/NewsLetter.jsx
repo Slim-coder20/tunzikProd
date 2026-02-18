98d73c807e7a559a5169362059302453016e8de3
Slim Abida <slimabida21@gmail.com>
2026-02-18 23:20:14 +0100

    Feat: implémentation de la section newsLetter dans le homePage

---
+++ client/src/components/NewsLetter.jsx
@@ -0,0 +1,45 @@
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+export default function NewLetter() {
+  // useForm pour gérer le formulaire de newsletter
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    defaultValues: {
+      email:"",
+    }
+  });
+
+  // handleSubmit pour gérer la soumission du formulaire
+  const onSubmit = (data) => {
+    console.log(data);
+    toast.success("Email enregistré avec succès");
+    reset();
+  };
+  return (
+    <>
+      <div className="w-full bg-slate-900 px-2 text-center text-white py-20 flex flex-col items-center justify-center">
+        <p className="text-indigo-500 font-medium">Restez informés</p>
+        <h1 className="max-w-lg font-semibold text-4xl/[44px] mt-2">
+          Inscrivez-vous à notre newsletter
+        </h1>
+        <div className="flex items-center justify-center mt-10 border border-slate-600 focus-within:outline focus-within:outline-indigo-600 text-sm rounded-full h-14 max-w-md w-full">
+          <input
+            {...register("email", { required: "Email est requis"})}
+            type="text"
+            className="bg-transparent outline-none rounded-full px-4 h-full flex-1"
+            placeholder="Entrer votre email"
+          />
+          {errors.email && (
+          <p className="text-red-500 text-xs mt-1 text-left pl-1">
+            {errors.email.message}
+          </p>
+          )}
+          <button className="bg-blue-600 text-white rounded-full h-11 mr-1 px-8 flex items-center justify-center cursor-pointer"
+          onClick={handleSubmit(onSubmit)}
+          >
+            S'abonner
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import HeroSection from "../components/HeroSection"
 import LatestRelease from "../components/LatestRelease"
 import About from "../components/About"
+import NewLetter from "../components/NewsLetter";
 const HomePage = () => {
   return (
     <div>
       {/* Contenu de la page d'accueil */}
-      <HeroSection/>
-      <LatestRelease/>
-      <About/>
-    </div>  
-  )
+      <HeroSection />
+      <LatestRelease />
+      <About />
+      <NewLetter />
+    </div>
+  );
 }
 
 export default HomePage
