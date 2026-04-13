fd8d7bc186478b9bf6f5c20f3464f114417acfea
Slim Abida <slimabida21@gmail.com>
2026-04-14 00:18:22 +0200

    Fix: correction du path du fichier email.js

--- server/config/email.js
+++ server/config/email.js
@@ -1,10 +1,10 @@
-import nodemailer from "nodemailer"; 
-import dotenv from "dotenv"; 
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
-// Création du transporter // 
-// Configuration fléxible pour le dév et la production // 
+// Création du transporter //
+// Configuration fléxible pour le dév et la production //
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp.gmail.com",
   port: parseInt(process.env.SMTP_PORT || "587", 10),
@@ -13,6 +13,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
-}); 
+});
 
-export default transporter; 
+export default transporter;
